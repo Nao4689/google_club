d9d11c3cbe3cda8f338d51b354a2b282ab2a0f91
Nao4689 <124106421+Nao4689@users.noreply.github.com>
2023-01-31 19:13:21 -0500

    Create FtoC.py
    Hs project Celsius calc

---
+++ FtoC.py
@@ -0,0 +1,6 @@
+def konvert():
+    fahrenheit = eval(input("Enter temperature in fahrenheit: "))
+    celsius = (fahrenheit - 32) * 5/9
+    print('%.2f Fahrenheit is: %0.2f Celsius' %(fahrenheit, celsius))
+    
+konvert() 
